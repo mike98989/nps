02e1b2b55be8a8002505e087a4563a10e74f28b9
Michael <mike98989@gmail.com>
2018-03-09 11:08:55 +0100

    new changes

--- config/paths.php
+++ config/paths.php
@@ -3,6 +3,7 @@
 define('URL', 'http://'.$_SERVER['HTTP_HOST'].'/nps/');
 define('UPLOAD_SIZE', 2097152); // 2MB
 define('UPLOAD_DIR', "{$_SERVER['DOCUMENT_ROOT']}/prison_cms_files");
+/*
 define('UPLOAD_TYPES', array(
 	'application/pdf',
 	'application/msword', // .doc
@@ -10,5 +11,6 @@ define('UPLOAD_TYPES', array(
 	'image/jpeg',
 	'image/jpg',
 	'image/png'));
+	*/
 
 //define('URL', 'http://'.$_SERVER['HTTP_HOST'].'/');
