5cb4bcd9ecf6f8a65cb97c50358078ad6175ff5b
Uk <ukchukundah@gmail.com>
2018-05-01 13:42:29 +0100

    Cache data

--- config/paths.php
+++ config/paths.php
@@ -3,3 +3,5 @@
 define('URL', 'http://'.$_SERVER['HTTP_HOST'].'/nps/');
 define('UPLOAD_SIZE', 2097152); // 2MB
 define('UPLOAD_DIR', "{$_SERVER['DOCUMENT_ROOT']}/prison_cms_files");
+define('MEMCACHED_HOST', "127.0.0.1");
+define('MEMCACHED_PORT', "11211");
